7293d20dbe6db822e64a815d5407cb0e258c7a1f
Tane4ka170 <tane4ka170@gmail.com>
2024-10-24 20:56:43 +0300

    Removed unnecessary comment

--- database/db.js
+++ database/db.js
@@ -10,7 +10,6 @@ mongoose
   .connect(mongoURI, options)
   .then(() => {
     console.log("Connected to MongoDB");
-    // Start your application or perform additional operations
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
