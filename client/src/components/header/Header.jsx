e6a67f9c298c327758a6f18ea3ddbb5fa9d11478
Tane4ka170 <tane4ka170@gmail.com>
2024-10-24 20:50:45 +0300

    Created Header component with logo

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="header">
+        <div className="header__logo">
+          <h3>TaskManager</h3>
+        </div>
+        <div className="header__buttons"></div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
