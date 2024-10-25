3e405aa237770c5f9478968d973469a2e4600e3f
Tane4ka170 <tane4ka170@gmail.com>
2024-10-25 16:28:49 +0300

    CreatedSignupcomponent

---
+++ client/src/components/registration/Signup.jsx
@@ -0,0 +1,13 @@
+const Signup = () => {
+  return (
+    <div>
+      <div className="signup-form">
+        <div className="form-group">
+          <input type="text" placeholder="Enter Name" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
