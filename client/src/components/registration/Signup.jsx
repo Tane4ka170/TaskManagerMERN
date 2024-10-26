a344c75cd05be3bb7653eb7ed1bbf394c548b279
Tane4ka170 <tane4ka170@gmail.com>
2024-10-26 20:04:21 +0300

    Added basic signup form

--- client/src/components/registration/Signup.jsx
+++ client/src/components/registration/Signup.jsx
@@ -1,9 +1,28 @@
+import "./registration.scss";
+
 const Signup = () => {
   return (
     <div>
       <div className="signup-form">
-        <div className="form-group">
-          <input type="text" placeholder="Enter Name" />
+        <div className="signup-form__wrapper">
+          <form>
+            <div className="form-group">
+              <input type="text" placeholder="Enter Name" />
+              <input
+                type="email"
+                name="Enter Name"
+                id=""
+                placeholder="Enter Email"
+              />
+              <input
+                type="password"
+                name="Enter Password"
+                id=""
+                placeholder="Enter Password"
+              />
+              <button>Sign Up</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
