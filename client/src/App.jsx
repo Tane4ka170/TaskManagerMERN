4e3c55fa1d184e59ea46f4cb7f070d82e3cebbe1
Tane4ka170 <tane4ka170@gmail.com>
2024-10-26 20:02:19 +0300

    Added Signup component to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Signup from "./components/registration/SignUp";
 
 function App() {
   return (
     <div>
       <Header />
+      <Signup />
     </div>
   );
 }
