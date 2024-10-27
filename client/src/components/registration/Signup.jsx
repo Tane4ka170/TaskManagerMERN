e5ee44ddfc5d233dfc71497838c4520302754d9c
Tane4ka170 <tane4ka170@gmail.com>
2024-10-27 17:32:16 +0200

    UpdatedSignupformstructureinSignup.jsx

--- client/src/components/registration/Signup.jsx
+++ client/src/components/registration/Signup.jsx
@@ -2,28 +2,28 @@ import "./registration.scss";
 
 const Signup = () => {
   return (
-    <div>
-      <div className="signup-form">
-        <div className="signup-form__wrapper">
-          <form>
-            <div className="form-group">
-              <input type="text" placeholder="Enter Name" />
-              <input
-                type="email"
-                name="Enter Name"
-                id=""
-                placeholder="Enter Email"
-              />
-              <input
-                type="password"
-                name="Enter Password"
-                id=""
-                placeholder="Enter Password"
-              />
-              <button>Sign Up</button>
-            </div>
-          </form>
-        </div>
+    <div className="signup-form">
+      <div className="signup-form__wrapper">
+        <form className="form">
+          <h4>Sign up</h4>
+
+          <div className="form-group">
+            <input type="text" placeholder="Enter Name" name="username" />
+          </div>
+          <div className="form-group">
+            <input type="email" name="email" placeholder="Enter Email" />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="form-group">
+            <button className="button">Sign Up</button>
+          </div>
+        </form>
       </div>
     </div>
   );
