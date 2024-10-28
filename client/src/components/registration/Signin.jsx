c771df74f9d71ae12cd0b1758f590de4ef28ff6e
Tane4ka170 <tane4ka170@gmail.com>
2024-10-28 22:46:05 +0200

    Added Signin form

--- client/src/components/registration/Signin.jsx
+++ client/src/components/registration/Signin.jsx
@@ -1,5 +1,30 @@
 const Signin = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="signup-form">
+      <div className="signup-form__wrapper">
+        <form className="form">
+          <h4>Sign up</h4>
+
+          <div className="form-group">
+            <input type="text" placeholder="Enter Name" name="username" />
+          </div>
+          <div className="form-group">
+            <input type="email" name="email" placeholder="Enter Email" />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="form-group">
+            <button className="button">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
