1731bb1eb88782a168e8376e49d02a7c311f69bc
Tane4ka170 <tane4ka170@gmail.com>
2024-10-29 20:40:46 +0200

    AddedRouterandRoutesinApp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Signin from "./components/registration/SignIn";
 import Signup from "./components/registration/SignUp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./styles/main.scss";
 
@@ -8,7 +10,12 @@ function App() {
   return (
     <div>
       <Header />
-      <Signup />
+      <Router>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
