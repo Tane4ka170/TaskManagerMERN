f2c6e2cb3eb91a2a1a3916e31c335b9826f13598
Tane4ka170 <tane4ka170@gmail.com>
2024-10-29 20:51:51 +0200

    UpdatedSigninheaderandformfields

--- client/src/components/registration/Signin.jsx
+++ client/src/components/registration/Signin.jsx
@@ -3,11 +3,8 @@ const Signin = () => {
     <div className="signup-form">
       <div className="signup-form__wrapper">
         <form className="form">
-          <h4>Sign up</h4>
+          <h4>Sign In</h4>
 
-          <div className="form-group">
-            <input type="text" placeholder="Enter Name" name="username" />
-          </div>
           <div className="form-group">
             <input type="email" name="email" placeholder="Enter Email" />
           </div>
