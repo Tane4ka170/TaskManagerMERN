861bb8fa5a6b1e6ee816f810b21f3ae5ff863f48
Tane4ka170 <tane4ka170@gmail.com>
2024-10-30 23:45:44 +0200

    added-state-signup-form

--- client/src/components/registration/Signup.jsx
+++ client/src/components/registration/Signup.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
 import "./registration.scss";
+import "../../styles/components/_button.scss";
 
 const Signup = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="signup-form">
       <div className="signup-form__wrapper">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <h4>Sign up</h4>
 
           <div className="form-group">
-            <input type="text" placeholder="Enter Name" name="username" />
+            <input
+              type="text"
+              placeholder="Enter Name"
+              name="username"
+              value={state.username}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group">
-            <input type="email" name="email" placeholder="Enter Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              value={state.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group">
             <input
               type="password"
               name="password"
               placeholder="Enter Password"
+              value={state.password}
+              onChange={handleChange}
             />
           </div>
           <div className="form-group">
