400e0d6afce2ce8f5af808935a9ea83ddf807ae6
Tane4ka170 <tane4ka170@gmail.com>
2024-11-01 21:44:05 +0200

    Added sign-in and registration functions with basic validation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,4 +2,24 @@ const User = require("../../database/model/user.model");
 const jwt = require("jsonwebtoken");
 const validator = require("email-validator");
 
-const signIn = (req, res) => {};
+const signIn = (req, res) => {
+  let { email, password } = req.body;
+};
+
+const register = async (req, res) => {
+  const { email, password, username } = req.body;
+  try {
+    if (!username)
+      return res.status(400).send("Username is required to proceed");
+
+    if (!email) return res.status(400).send("Email is required to proceed");
+
+    if (!validator.validate(email))
+      return res.status(400).send("Please enter a valid email address");
+
+    if (!password || password.length < 6)
+      return res.status(400).send("Please enter a valid password");
+
+    const userExist = await User.findOne();
+  } catch (error) {}
+};
