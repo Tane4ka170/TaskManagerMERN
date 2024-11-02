372fa066b28366ab6df55b7259ba227c8dafff5e
Tane4ka170 <tane4ka170@gmail.com>
2024-11-02 22:54:39 +0200

    Added_signin_endpoint_to_auth_routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,1 +1,6 @@
 const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+router.route("/signin").post(authController.signIn);
