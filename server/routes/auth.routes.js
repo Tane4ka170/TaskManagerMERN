5535b1ceab90cb21aa2abe84e38c0ce70b8326a0
Tane4ka170 <tane4ka170@gmail.com>
2024-11-03 23:32:47 +0200

    Added signup route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,3 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.route("/signin").post(authController.signIn);
+router.route("/signup").post(authController.register);
+
+module.exports = router;
