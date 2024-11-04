8f13d683ecb734242a64b586f7ae22c4c5e8d849
Tane4ka170 <tane4ka170@gmail.com>
2024-11-04 16:47:23 +0200

    Enhancedauthslicewithregister&signinlogic

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import history from "../history";
 
 const initialState = {
   isLoading: false,
@@ -14,7 +16,6 @@ export const authSlice = createSlice({
       state.currentUser = action.payload;
       state.isLoading = false;
     },
-
     loginFailure: (state, action) => {
       state.error = action.payload;
     },
@@ -22,11 +23,10 @@ export const authSlice = createSlice({
       state.currentUser = action.payload;
       state.isLoading = false;
     },
-
     registerFailure: (state, action) => {
       state.error = action.payload;
     },
-    logoutSuccess: (state, action) => {
+    logoutSuccess: (state) => {
       state.currentUser = null;
     },
   },
@@ -42,24 +42,51 @@ export const {
 
 export default authSlice.reducer;
 
-export const register = (user) => async (dispatch){
-  
-
+export const register = (user) => async (dispatch) => {
   try {
-    const formData = new FormData()
+    const formData = new FormData();
+    formData.append("username", user.username);
+    formData.append("email", user.email);
+    formData.append("password", user.password);
 
-      formData.append('username', user.username)
-  formData.append('email', user.email)
-    formData.append('password', user.password)
-    
     const config = {
       headers: {
-        'content-type': 'application/json'
-      }
+        "content-type": "application/json",
+      },
+    };
+
+    const response = await axios.post(
+      "http://localhost:8325/auth/register",
+      formData,
+      config
+    );
+    if (response) {
+      dispatch(registerSuccess(response.data));
+      history.push("/signin");
+      window.location.reload();
+    } else {
+      dispatch(registerFailure("Registration failed"));
     }
   } catch (error) {
-    
+    dispatch(registerFailure(error.message));
   }
+};
 
-
+export const signin = (user) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:8325/auth/signin",
+      user
+    );
+    if (response) {
+      localStorage.setItem("auth", JSON.stringify(response.data));
+      dispatch(loginSuccess(response.data));
+      history.push("/dashboard");
+      window.location.reload();
+    } else {
+      dispatch(loginFailure("Login failed"));
+    }
+  } catch (error) {
+    dispatch(loginFailure(error.message));
+  }
 };
