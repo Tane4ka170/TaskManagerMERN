1866c5523fb6f9c67cb9cf3a8850be8892ec4cab
Tane4ka170 <tane4ka170@gmail.com>
2024-11-05 18:36:03 +0200

    Added conditional rendering & logout functionality

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,21 @@
 import "./header.scss";
+import "../../styles/components/_button.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { logoutSuccess } from "../../redux/authSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(logoutSuccess());
+    localStorage.removeItem("auth");
+    history.push("/signin");
+    window.location.reload();
+  };
+
   return (
     <div>
       <nav className="header">
@@ -8,8 +23,20 @@ const Header = () => {
           <h3>TaskManager</h3>
         </div>
         <div className="header__buttons">
-          <button>Sign in</button>
-          <button>Sign Out</button>
+          {auth.currentUser && auth.currentUser.token ? (
+            <Link to="/signin" className="button" onClick={handleClick}>
+              SignOut
+            </Link>
+          ) : (
+            <>
+              <Link to="/signin" className="button">
+                SignIn
+              </Link>
+              <Link to="/signup" className="button">
+                SignUp
+              </Link>
+            </>
+          )}
         </div>
       </nav>
     </div>
