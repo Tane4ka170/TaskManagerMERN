930efc0a1972840a9a2b8b293d6946f591d053bf
Tane4ka170 <tane4ka170@gmail.com>
2024-11-05 18:39:04 +0200

    Added conditional rendering for home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,29 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import "./home.scss";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { currentUser } = auth;
+
+  return (
+    <div className="home">
+      <div className="home__container">
+        <h2>Keep everything organized</h2>
+        <p>With TaskManager</p>
+        {currentUser && currentUser.token ? (
+          <Link to="/dashboard" className="button">
+            Begin now
+          </Link>
+        ) : (
+          <Link to="/signin" className="button">
+            Begin now
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
