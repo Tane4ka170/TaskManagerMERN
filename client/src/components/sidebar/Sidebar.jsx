eade6b456622088ff27245339aa5838a9066d1aa
Tane4ka170 <tane4ka170@gmail.com>
2024-11-08 17:25:46 +0200

    Enhanced sidebar with user info and dashboard link

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,24 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 import "./sidebar.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { currentUser } = auth;
+
+  return (
+    <div>
+      <ul className="sidebar">
+        <li className="list-item">
+          <h5>{currentUser.username}</h5>
+        </li>
+        <li className="list-item">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
