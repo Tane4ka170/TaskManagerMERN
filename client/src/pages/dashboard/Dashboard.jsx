b75284118f3340fe72f6e13fd8932d03b12a4798
Tane4ka170 <tane4ka170@gmail.com>
2024-11-08 16:24:06 +0200

    Created basic dashboard layout

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./dashboard.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="dashboard">
+        <div className="dashboard__left">
+          <Sidebar />
+        </div>
+        <div className="dashboard__right">
+          <div className="taskcount">
+            <div className="todo box">todo</div>
+            <div className="doing box">doing</div>
+            <div className="done box">done</div>
+          </div>
+          <div className="createButton">
+            <Link to="/taskmanager" className="button">
+              Create Task
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
