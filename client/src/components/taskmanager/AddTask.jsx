0c3a84c3b91922c33d067082b02c068e5f7e0098
Tane4ka170 <tane4ka170@gmail.com>
2024-11-09 13:52:14 +0200

    Addedform&statehandlingtoAddTaskcomponent

--- client/src/components/taskmanager/AddTask.jsx
+++ client/src/components/taskmanager/AddTask.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import "./addtask.scss";
 
 const AddTask = () => {
-  return <div>AddTask</div>;
+  const [state, setState] = useState({
+    task: "",
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {};
+  return (
+    <div>
+      <div className="addtask">
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="task"
+            placeholder="Add your task now"
+            onChange={handleChange}
+            value={state.task}
+          />
+          <button className="button">Add Task</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddTask;
