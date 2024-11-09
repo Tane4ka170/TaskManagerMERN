65bdbb3b9462d649ce1fc89059f4d30ee955e036
Tane4ka170 <tane4ka170@gmail.com>
2024-11-09 16:19:21 +0200

    Added Redux integration for task submission

--- client/src/components/taskmanager/AddTask.jsx
+++ client/src/components/taskmanager/AddTask.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./addtask.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask } from "../../redux/taskSlice";
 
 const AddTask = () => {
   const [state, setState] = useState({
     task: "",
   });
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { currentUser } = auth;
 
   const handleChange = (e) => {
     setState({
@@ -13,7 +18,14 @@ const AddTask = () => {
     });
   };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTask(state.task, currentUser._id));
+    setState({
+      task: "",
+    });
+  };
+
   return (
     <div>
       <div className="addtask">
