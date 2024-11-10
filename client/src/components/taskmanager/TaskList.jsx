7d8579250fea2ae2f27441b30e9b5e0c1c7b0334
Tane4ka170 <tane4ka170@gmail.com>
2024-11-10 13:54:40 +0200

    Integrated Redux and rendered task list

--- client/src/components/taskmanager/TaskList.jsx
+++ client/src/components/taskmanager/TaskList.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTasks } from "../../redux/taskSlice";
+import ListCard from "./ListCard";
 
 const TaskList = () => {
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const tasks = useSelector((state) => state.task);
+  const { currentUser } = auth;
+  const AllTasks = tasks?.AllTasks || {};
 
   useEffect(() => {
-    dispatch(getAllTasks());
-  });
-  return <div>TaskList</div>;
+    if (currentUser?.token) {
+      dispatch(getAllTasks(currentUser.token));
+    }
+  }, [dispatch, currentUser?.token]);
+
+  return (
+    <div>
+      {Object.values(AllTasks).map((item) => {
+        return <ListCard />;
+      })}
+    </div>
+  );
 };
 
 export default TaskList;
