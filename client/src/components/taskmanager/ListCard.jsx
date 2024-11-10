f281ea500c545c18ab43f1c986563517fa4db65a
Tane4ka170 <tane4ka170@gmail.com>
2024-11-10 18:49:06 +0200

    Here's a commit message for client/src/components/taskmanager/ListCard.jsx in under 50 characters (without spaces):  Rendered task details in ListCard

--- client/src/components/taskmanager/ListCard.jsx
+++ client/src/components/taskmanager/ListCard.jsx
@@ -1,5 +1,16 @@
+import "./listcard.scss";
+
 const ListCard = ({ item }) => {
-  return <div>ListCard</div>;
+  return (
+    <div>
+      <ul className="menu">
+        <li>{item._id}</li>
+        <li>{item.task}</li>
+        <li>{item.status}</li>
+        <li>{item.action}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default ListCard;
