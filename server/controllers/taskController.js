a2d79edaeeff2a9cd1505ea8cdbeff472ff3f0d8
Tane4ka170 <tane4ka170@gmail.com>
2024-11-10 13:58:26 +0200

    Added task CRUD & auth (getAllTasks)

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -21,4 +21,69 @@ const addTask = async (req, res) => {
   }
 };
 
-module.exports = { addTask };
+const getAllTasks = async (req, res) => {
+  const { id } = req.query;
+  try {
+    let tasklist = await Task.find({ cretedBy: id });
+    return res.status(200).send(tasklist);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+const editTask = async (req, res) => {};
+
+const statusChange = async (req, res) => {
+  const { id, string } = req.body;
+
+  try {
+    let task = await Task.findById({ _id: id });
+    if (string === "right") {
+      if (task.status === "backlog") {
+        task.status = "todo";
+        task.save();
+        return res.send(task);
+      } else if (task.status === "todo") {
+        task.status = "doing";
+        task.save();
+        return res.send(task);
+      } else if (task.status === "doing") {
+        task.status = "done";
+        task.save();
+        return res.send(task);
+      }
+    } else {
+      if (task.status === "done") {
+        task.status = "doing";
+        task.save();
+        return res.send(task);
+      } else if (task.status === "doing") {
+        task.status = "todo";
+        task.save();
+        return res.send(task);
+      } else if (task.status === "todo") {
+        task.status = "backlog";
+        task.save();
+        return res.send(task);
+      }
+    }
+  } catch (error) {}
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let response = await Task.findByIdAndDelete(id);
+    return res.status(200).send(response);
+  } catch (error) {
+    return res.status(400).send("Deletion unsuccessful");
+  }
+};
+
+module.exports = {
+  addTask,
+  getAllTasks,
+  editTask,
+  statusChange,
+  deleteTask,
+};
