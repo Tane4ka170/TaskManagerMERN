3c6634e0d7662d750f88727e6972910c1279b59c
Tane4ka170 <tane4ka170@gmail.com>
2024-11-10 18:51:13 +0200

    Improved task list display & headers

--- client/src/components/taskmanager/TaskList.jsx
+++ client/src/components/taskmanager/TaskList.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTasks } from "../../redux/taskSlice";
 import ListCard from "./ListCard";
+import "./tasklist.scss";
 
 const TaskList = () => {
   const dispatch = useDispatch();
@@ -18,9 +19,23 @@ const TaskList = () => {
 
   return (
     <div>
-      {Object.values(AllTasks).map((item) => {
-        return <ListCard />;
-      })}
+      <ul className="list-header">
+        <li>
+          <h5>Id</h5>
+        </li>
+        <li>
+          <h5>Issue Name</h5>
+        </li>
+        <li>
+          <h5>Status</h5>
+        </li>
+        <li>
+          <h5>Action</h5>
+        </li>
+      </ul>
+      {Object.values(AllTasks).map((item) => (
+        <ListCard key={item._id} item={item} />
+      ))}
     </div>
   );
 };
