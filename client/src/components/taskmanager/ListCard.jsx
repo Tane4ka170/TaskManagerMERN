6cc94e756aeecfa1744c43cf9dacca4e966eae54
Tane4ka170 <tane4ka170@gmail.com>
2024-11-11 23:13:57 +0200

    Updated ListCard to handle task deletion and status changes

--- client/src/components/taskmanager/ListCard.jsx
+++ client/src/components/taskmanager/ListCard.jsx
@@ -1,27 +1,47 @@
 import "./listcard.scss";
 import { BiChevronLeft, BiChevronRight, BiTrash } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { arrowClick } from "../../redux/taskSlice";
+import { arrowClick, deleteItem } from "../../redux/taskSlice";
 
-const ListCard = ({ item }) => {
+const ListCard = (items) => {
+  const { item } = items;
+
   const dispatch = useDispatch();
 
-  const ArrowClick = (item) => {
-    dispatch(arrowClick(item));
+  const arrrowClick = (string) => {
+    dispatch(arrowClick(item, string));
   };
-
+  const handleDelete = () => {
+    dispatch(deleteItem(item._id));
+  };
   return (
     <div>
-      <ul className="menu">
-        <li>{item._id}</li>
-        <li>{item.task}</li>
-        <li>{item.status}</li>
+      <ul className={` ${item.status === "done" ? "completed menu" : "menu"}`}>
         <li>
-          <button disabled={item.status === "backlog"} onClick={ArrowClick}>
+          <p>{item._id}</p>
+        </li>
+        <li>
+          <p>{item.task}</p>
+        </li>
+        <li>
+          <p>{item.status}</p>
+        </li>
+        <li>
+          <button
+            disabled={item.status === "backlog"}
+            onClick={() => arrrowClick("left")}
+          >
             <BiChevronLeft />
           </button>
-          <button disabled={item.status === "done"} onClick={ArrowClick}>
+
+          <button
+            disabled={item.status === "done"}
+            onClick={() => arrrowClick("right")}
+          >
             <BiChevronRight />
+          </button>
+          <button onClick={handleDelete}>
+            <BiTrash />
           </button>
         </li>
       </ul>
