0584432cf9047879142c7fd66155fb4fb6aae708
Tane4ka170 <tane4ka170@gmail.com>
2024-11-11 17:34:32 +0200

    Added Redux dispatch for arrow click functionality in ListCard

--- client/src/components/taskmanager/ListCard.jsx
+++ client/src/components/taskmanager/ListCard.jsx
@@ -1,14 +1,29 @@
 import "./listcard.scss";
 import { BiChevronLeft, BiChevronRight, BiTrash } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { arrowClick } from "../../redux/taskSlice";
 
 const ListCard = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const ArrowClick = (item) => {
+    dispatch(arrowClick(item));
+  };
+
   return (
     <div>
       <ul className="menu">
         <li>{item._id}</li>
         <li>{item.task}</li>
         <li>{item.status}</li>
-        <li>{item.action}</li>
+        <li>
+          <button disabled={item.status === "backlog"} onClick={ArrowClick}>
+            <BiChevronLeft />
+          </button>
+          <button disabled={item.status === "done"} onClick={ArrowClick}>
+            <BiChevronRight />
+          </button>
+        </li>
       </ul>
     </div>
   );
